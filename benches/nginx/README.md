12145c1b4e293db06aa505ea0a21270a6fdd6e85
Olivier Giniaux <oginiaux@gmail.com>
2025-09-20 16:13:36 +0200

    Add benchmark against nginx for simple caching (#4)
    * Bench vs nginx
    
    * Opt in for a local setup
    
    * Add today test reports

---
+++ benches/nginx/README.md
@@ -0,0 +1,5 @@
+sh```
+docker compose up -d
+k6 cloud login --token <token>
+k6 cloud run --local-execution script.js
+```
---
+++ benches/nginx/script.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+
+export const options = {
+  discardResponseBodies: true,
+  cloud: {
+    projectID: 4565599,
+    name: 'Nginx to Hello World',
+  },
+  scenarios: {
+    contacts: {
+      executor: 'ramping-arrival-rate',
+      // Start iterations per `timeUnit`
+      startRate: 100,
+      // Start `startRate` iterations per minute
+      timeUnit: '1s',
+      // Pre-allocate necessary VUs.
+      preAllocatedVUs: 100,
+      stages: [
+        { target: 20000, duration: '5m' },
+      ],
+    },
+  },
+};
+
+export default function() {
+  http.get('http://127.0.0.1:8080');
+}
