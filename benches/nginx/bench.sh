cb78ff78262b43a7e021baf2fa6eea9074e94d60
Olivier Giniaux <oginiaux@smartadserver.com>
2025-09-20 14:07:35 +0200

    Opt in for a local setup

--- benches/nginx/bench.sh
+++ benches/nginx/bench.sh
@@ -3,7 +3,10 @@ k6 run script.js
 docker stop nginx-cache
 docker rm nginx-cache
 
-docker stats nginx-cache
+podman stats nginx-cache
 
-
+podman run -d --name cacheus -p 8081:80 -v $(pwd)/cacheus.yml:/etc/cacheus.yml ghcr.io/ogxd/cacheus:0.1.0
 docker run -d --name cacheus -p 8081:80 -v $(pwd)/cacheus.yml:/etc/cacheus.yml ghcr.io/ogxd/cacheus:0.1.0
+
+
+podman run -d --name nginx-cache -p 8080:80 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro -v $(pwd)/cache:/var/cache/nginx docker.io/library/nginx
